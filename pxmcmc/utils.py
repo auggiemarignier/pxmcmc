4f08e2415b7c912fa96f43b8fc736266ef671193
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-05-08 21:34:31 +0100

    utils tested

---
+++ pxmcmc/utils.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+
+def flatten_mlm(wav_lm, scal_lm):
+    """
+    Takes a set of wavelet and scaling coefficients and flattens them into a single vector
+    """
+    buff = wav_lm.ravel(order="F")
+    mlm = np.concatenate((scal_lm, buff))
+    return mlm
+
+
+def expand_mlm(mlm, nscales):
+    """
+    Sepatates scaling and wavelet coefficients from a single vector to separate arrays.
+    """
+    v_len = mlm.size // (nscales + 1)
+    assert v_len > 0
+    scal_lm = mlm[:v_len]
+    wav_lm = np.zeros((v_len, nscales), dtype=np.complex)
+    for i in range(nscales):
+        wav_lm[:, i] = mlm[(i + 1) * v_len : (i + 2) * v_len]
+    return wav_lm, scal_lm
+
+
+def soft(X, T=0.1):
+    """
+    Soft thresholding of a vector X with threshold T.  If Xi is less than T, then soft(Xi) = 0, otherwise soft(Xi) = Xi-T.
+    """
+    t = np.zeros_like(X)
+    for i, x in enumerate(X):
+        if abs(x) == 0:
+            continue
+        t[i] = x * max(abs(x) - T, 0) / abs(x)
+    return t
+
+
+def hard(X, T=0.1):
+    """
+    Hard thresholding of a vector X with fraction threshold T. T is the fraction kept, i.e. the largest 100T% absolute values are kept, the others are thresholded to 0.
+    TODO: What happens when all elements of X are equal?
+    """
+    X_srt = np.sort(abs(X))
+    thresh_ind = int(T * len(X))
+    thresh_val = X_srt[-thresh_ind]
+    X[abs(X) < thresh_val] = 0
+    return X
---
+++ tests/test_utils.py
@@ -0,0 +1,30 @@
+from pxmcmc import utils
+import numpy as np
+import pytest
+
+
+def test_flattenmlm():
+    f_wav_lm = np.ones((861, 9))
+    f_scal_lm = np.ones((861,))
+    assert all(utils.flatten_mlm(f_wav_lm, f_scal_lm) == np.ones((8610,)))
+
+
+def test_expandmlm():
+    mlm = np.ones((8610,))
+    f_wav_lm, f_scal_lm = utils.expand_mlm(mlm, 9)
+    assert f_wav_lm.shape == (861, 9)
+    assert f_scal_lm.shape == (861,)
+
+
+@pytest.mark.parametrize(
+    "ins,thresh,outs", [([1, 2, 3], 2, [0, 0, 1]), ([-1, -2, -3], 2, [0, 0, -1])]
+)
+def test_soft(ins, thresh, outs):
+    assert all(utils.soft(ins, T=thresh) == outs)
+
+
+@pytest.mark.parametrize(
+    "ins,thresh,outs", [(np.arange(1, 11), 0.3, [0, 0, 0, 0, 0, 0, 0, 8, 9, 10])]
+)
+def test_hard(ins, thresh, outs):
+    assert all(utils.hard(ins, T=thresh) == outs)
