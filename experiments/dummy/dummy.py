acb91b11cebf7e5499d223affee72b1a72f1599b
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-05-11 12:44:03 +0100

    simpledata_lm now has negative ems

--- experiments/dummy/dummy.py
+++ experiments/dummy/dummy.py
@@ -1,20 +1,25 @@
 import numpy as np
 import healpy as hp
+import pys2let
 
 from pxmcmc.mcmc import PxMCMC, PxMCMCParams
 from pxmcmc.forward import ForwardOperator, ISWTOperator
 from pxmcmc.saving import Outfile
 
 
-def simpledata(Nside, sig_d):
+def simpledata(Nside, sig_d, complex=False):
     simple = np.full(hp.nside2npix(Nside), 10)
     noise = np.random.normal(scale=sig_d, size=simple.shape)
+    if complex:
+        simple = simple + 1j * simple
+        noise = noise + 1j * noise
     return simple + noise
 
 
-def simpledata_lm(Nside, sig_d, L):
-    realspace = simpledata(Nside, sig_d)
+def simpledata_lm(Nside, sig_d, L, complex=False):
+    realspace = simpledata(Nside, sig_d, complex=complex)
     alm = hp.map2alm(realspace, lmax=L)
+    alm = pys2let.lm_hp2lm(alm, L)
     return alm
 
 
@@ -31,7 +36,14 @@ def simpledata_lm(Nside, sig_d, L):
 data = simpledata_lm(4, 0.01, 10)
 forwardop = ISWTOperator(data, 0.01, 10, 1.5, 2)
 params = PxMCMCParams(
-    nsamples=int(1e6), nburn=0, ngap=0, nparams=forwardop.nparams, complex=True, delta=1e-7, lmda=3e-7, mu=1e-7
+    nsamples=int(1e6),
+    nburn=0,
+    ngap=0,
+    nparams=forwardop.nparams,
+    complex=True,
+    delta=1e-7,
+    lmda=3e-7,
+    mu=1e-7,
 )
 mcmc = PxMCMC(forwardop, params)
 mcmc.mcmc()
