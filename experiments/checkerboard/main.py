33e3a16889f979cbb60abba652b3566adda380ba
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-05-21 14:05:28 +0100

    noisy checkerboard run

--- experiments/checkerboard/main.py
+++ experiments/checkerboard/main.py
@@ -11,27 +11,35 @@ root_dir = "/Users/auggiemarignier/Documents/PhD/PxMCMC/experiments/checkerboard
 L = 15
 B = 1.5
 J_min = 2
-sig_d = 0.01
+sig_d = 0.03
 true_model = hp.read_map(f"{root_dir}/chkrbrd30.fits", verbose=False)
 alms = pys2let.lm_hp2lm(hp.map2alm(true_model, lmax=L), L + 1)
 noise = np.random.normal(scale=sig_d, size=alms.shape)
-# data = alms + noise
+data = alms + noise
 data = alms
 
 forwardop = ISWTOperator(data, sig_d, L, B, J_min)
 params = PxMCMCParams(
-    nsamples=int(1e6),
+    nsamples=int(5e5),
     nburn=0,
     ngap=0,
     complex=True,
-    delta=1e-7,
+    delta=5e-8,
     lmda=3e-7,
-    mu=1e-7,
+    mu=1e2,
     verbosity=1000,
 )
 mcmc = PxMCMC(forwardop, params, X_func=None)
 mcmc.pxmala()
 
 save_mcmc(
-    mcmc, params, "/Volumes/Elements", L=L, B=B, J_min=J_min, sig_d=sig_d, nparams=forwardop.nparams
+    mcmc,
+    params,
+    "/Volumes/Elements/PxMCMCoutputs/checkerboard",
+    filename="pxmala_n",
+    L=L,
+    B=B,
+    J_min=J_min,
+    sig_d=sig_d,
+    nparams=forwardop.nparams,
 )
