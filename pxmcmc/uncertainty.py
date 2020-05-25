b212be3577d1751dc714dbfe645b2720875ddfa1
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-05-25 17:11:05 +0100

    credible regions

--- pxmcmc/uncertainty.py
+++ pxmcmc/uncertainty.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 
-def pixel_ci_range(predictions, alpha=0.05):
+def credible_interval_range(predictions, alpha=0.05):
     quantiles = np.quantile(predictions, (alpha / 2, 1 - alpha / 2), axis=0)
     return np.diff(quantiles, axis=0)[0]
+
+
+def credible_region_threshold(logpis, alpha=0.05):
+    return np.quantile(logpis, 1 - alpha)
+
+
+def in_credible_region(logpi, threshold):
+    return True if logpi <= threshold else False
