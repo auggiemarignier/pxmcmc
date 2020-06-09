85ee3a77dd356983fd906ce65762f1ff7a1f5ea5
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-06-09 11:00:38 +0100

    ISWTgradg tests

--- tests/test_forward.py
+++ tests/test_forward.py
@@ -29,7 +29,9 @@ def test_ISWTForward(iswtoperator, Nside):
     flm = pys2let.lm_hp2lm(flm_hp, L + 1)
 
     if iswtoperator.setting == "synthesis":
-        f_wav_lm_hp, f_scal_lm_hp = pys2let.analysis_axisym_lm_wav(flm_hp, B, L + 1, J_min)
+        f_wav_lm_hp, f_scal_lm_hp = pys2let.analysis_axisym_lm_wav(
+            flm_hp, B, L + 1, J_min
+        )
         f_wav_lm = np.zeros(((L + 1) ** 2, f_wav_lm_hp.shape[1]), dtype=np.complex)
         for j in range(iswtoperator.nscales):
             f_wav_lm[:, j] = pys2let.lm_hp2lm(
@@ -43,11 +45,28 @@ def test_ISWTForward(iswtoperator, Nside):
     assert np.allclose(iswtoperator.forward(X), flm)
 
 
+@pytest.mark.parametrzie("setting", ["analysis", "synthesis"], indirect=["setting"])
 def test_ISWTGradg(iswtoperator):
+    from pxmcmc.utils import flatten_mlm
     iswtoperator.sig_d = 1
-    iswtoperator.pf = np.ones(iswtoperator.nparams)
     preds = np.ones(len(iswtoperator.data))
-    expected = np.concatenate([1 - iswtoperator.data] * iswtoperator.basis.shape[1])
+    diff = preds - iswtoperator.data
+
+    if iswtoperator.setting == "synthesis":
+        f = pys2let.alm2map_mw(diff, iswtoperator.L, 0)
+        f_wav, f_scal = pys2let.synthesis_adjoint_axisym_wav_mw(
+            f, iswtoperator.B, iswtoperator.L, iswtoperator.J_min
+        )
+        f_scal_lm = pys2let.map2alm_mw(f_scal, iswtoperator.L, 0)
+        f_wav_lm = np.zeros(
+            [(iswtoperator.L + 1) ** 2, iswtoperator.nscales], dtype=np.complex
+        )
+        vlen = iswtoperator.L * (2 * iswtoperator.L - 1)
+        for j in range(iswtoperator.nscales):
+            f_wav_lm[:, j] = pys2let.map2alm_mw(f_wav[j * vlen : (j + 1) * vlen + 1])
+        expected = flatten_mlm(f_wav_lm, f_scal_lm)
+    else:
+        expected = np.copy(diff)
     assert np.allclose(iswtoperator.calc_gradg(preds), expected)
 
 
