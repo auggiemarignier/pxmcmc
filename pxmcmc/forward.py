14c0c12eaa825cfba711617e8c23af3cdb08eb1c
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-06-18 11:00:16 +0100

    GCPIntegralOperator reads/saves sparse path matrix

--- pxmcmc/forward.py
+++ pxmcmc/forward.py
@@ -1,6 +1,9 @@
 import pys2let
 import numpy as np
 import healpy as hp
+import os
+from scipy import sparse
+
 from pxmcmc.utils import expand_mlm, flatten_mlm, alm2map, map2alm, wavelet_basis
 
 
@@ -130,7 +133,7 @@ class SWC2PixOperator(ISWTOperator):
         self.Nside = Nside
 
         if setting == "analysis":
-            self.nparams = Nside
+            self.nparams = hp.nside2npix(Nside)
 
     def _forward_synthesis(self, X):
         clm = super()._forward_synthesis(X)
@@ -161,3 +164,82 @@ class SWC2PixOperator(ISWTOperator):
                 f_wav[j * vlen : (j + 1) * vlen + 1], self.L, 0
             )
         return flatten_mlm(f_wav_lm, f_scal_lm) / (self.sig_d ** 2)
+
+
+class GCPIntegralOperator(ForwardOperator):
+    def __init__(
+        self,
+        datafile,
+        Nside,
+        L,
+        B,
+        J_min,
+        setting,
+        dirs=1,
+        spin=0,
+        path_matrix_file=None,
+    ):
+        self.Nside = Nside
+        self.L = L
+        self.B = B
+        self.J_min = J_min
+        self.J_max = pys2let.pys2let_j_max(self.B, self.L, self.J_min)
+        self.nscales = self.J_max - self.J_min + 1
+        self.dirs = dirs
+        self.spin = spin
+
+        if setting == "synthesis":
+            basis = wavelet_basis(L, B, J_min, dirs, spin)
+            self.nparams = np.prod(basis.shape)
+        else:
+            self.nparams = hp.nside2npix(Nside)
+
+        self._read_datafile(datafile)
+        self._get_path_matrix(path_matrix_file)
+
+    def _read_datafile(self, datafile):
+        """
+        Expects a file with the following columns for each path:
+        start_lat, start_lon, stop_lat, stop_lon, data, error
+        Coordinates given in degrees
+        TODO: Check what happens when each data point has a different error
+        TODO: Figure out what to do with minor/major and nsim
+        """
+        all_data = np.np.loadtxt(datafile)
+        start_lat = all_data[:, 0]
+        start_lon = all_data[:, 1]
+        self.start = np.stack([start_lon, start_lat], axis=1)
+        stop_lat = all_data[:, 2]
+        stop_lon = all_data[:, 3]
+        self.stop = np.stack([stop_lon, stop_lat], axis=1)
+        self.data = all_data[:, 4]
+        sig_d = all_data[:, 5]
+        self.sig_d = np.max(sig_d)
+
+    def _get_path_matrix(self, path_matrix_file):
+        if path_matrix_file is None:
+            path_matrix_file = f"path_matrix_{self.Nside}.npz"
+        if os.path.exists(path_matrix_file):
+            self._read_path_matrix_file(path_matrix_file)
+        else:
+            self._build_path_matrix_file(path_matrix_file)
+
+    def _read_path_matrix_file(self, path_martix_file):
+        self.path_matrix = sparse.load_npz(path_martix_file)
+
+    def _build_path_matrix_file(self, path_matrix_file):
+        from pxmcmc.utils import GreatCirclePath
+        from multiprocessing import Pool
+
+        def build_path(start, stop):
+            path = GreatCirclePath(start, stop, self.Nside)
+            path.get_points(2000)
+            path.fill()
+            return path.map
+
+        itrbl = [(start, stop) for (start, stop) in zip(self.start, self.stop)]
+        with Pool() as p:
+            result = p.starmap_async(build_path, itrbl)
+            paths = result.get()
+        self.path_matrix = sparse.csr_matrix(paths)
+        sparse.save_npz(path_matrix_file, self.path_matrix)
