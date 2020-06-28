3233ffb662a7a5eaa012545292b93f1fe794ff59
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-06-28 14:55:18 +0100

    argparse in/out

--- experiments/earthtopography/main.py
+++ experiments/earthtopography/main.py
@@ -1,12 +1,20 @@
 import healpy as hp
 import datetime
+import argparse
+import os
 
 from pxmcmc.mcmc import MYULA, PxMALA, PxMCMCParams
 from pxmcmc.forward import SWC2PixOperator
 from pxmcmc.prox import L1
 from pxmcmc.saving import save_mcmc
 
-topo = hp.read_map("ETOPO1_Ice_hpx_256.fits", verbose=False)
+parser = argparse.ArgumentParser()
+parser.add_argument("--infile", type=str, default="ETOPO1_Ice_hpx_256.fits")
+parser.add_argument("--outdir", type=str, default=".")
+args = parser.parse_args()
+
+
+topo = hp.read_map(args.infile, verbose=False)
 sig_d = 0.03
 
 L = 16
@@ -19,11 +27,11 @@ forwardop = SWC2PixOperator(topo_d, sig_d, Nside, L, B, J_min, setting)
 params = PxMCMCParams(
     nsamples=int(1e3),
     nburn=0,
-    ngap=10,
+    ngap=100,
     complex=True,
     delta=2.5e-8,
     lmda=1e-7,
-    mu=1,
+    mu=1e-1,
     verbosity=100,
 )
 regulariser = L1(setting, None, None, params.lmda * params.mu)
@@ -40,7 +48,7 @@ save_mcmc(
     mcmc,
     params,
     ".",
-    filename=f"pxmala_{NOW.strftime('%d%m%y_%H%M%S')}",
+    filename=os.path.join(args.outdir, f"pxmala_{NOW.strftime('%d%m%y_%H%M%S')}"),
     L=L,
     B=B,
     J_min=J_min,
