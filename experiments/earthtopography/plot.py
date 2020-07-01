b2dc7b51a93b33821008a73407d67d11d595ecb0
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-07-01 14:21:26 +0100

    extra suffix option for figure filenames

--- experiments/earthtopography/plot.py
+++ experiments/earthtopography/plot.py
@@ -10,11 +10,12 @@ from pxmcmc import uncertainty
 parser = argparse.ArgumentParser()
 parser.add_argument("datafile", type=str)
 parser.add_argument("directory", type=str)
+parser.add_argument("--suffix", type=str, default="")
 args = parser.parse_args()
 
 
 def filename(name):
-    return f"{args.directory}/{name}"
+    return f"{args.directory}/{name}{args.suffix}.png"
 
 
 file = h5py.File(args.datafile, "r")
@@ -24,24 +25,24 @@ logpi = file["logposterior"][()]
 L2s = file["L2s"][()]
 L1s = file["L1s"][()]
 evo = plotting.plot_evolution(logpi, L2s, L1s)
-evo.savefig(filename("evolution.png"))
+evo.savefig(filename("evolution"))
 
 
 MAP_idx = np.where(logpi == max(logpi))
 MAP = file["predictions"][MAP_idx][0]
 maxapost = plotting.mollview(MAP, cmap=cm.jet, title="Maximum a posteriori solution")
-maxapost.savefig(filename("MAP.png"))
+maxapost.savefig(filename("MAP"))
 
 MAP_X = file["chain"][MAP_idx][0]
 mapx = plotting.plot_chain_sample(MAP_X)
-mapx.savefig(filename("MAP_X.png"))
+mapx.savefig(filename("MAP_X"))
 
 
 basis_els = plotting.plot_basis_els(
     file["chain"][750:], params["L"], params["B"], params["J_min"], inflate_mads=100
 )  # Careful with L
-basis_els.savefig(filename("basis_els.png"))
+basis_els.savefig(filename("basis_els"))
 
 ci_range = uncertainty.credible_interval_range(file["predictions"][750:])
 ci_map = plotting.mollview(ci_range, min=0, title="95% credible interval range")
-ci_map.savefig(filename("ci_map.png"))
+ci_map.savefig(filename("ci_map"))
