f04ccf90b8da07bb32fe7bf40fe1162288103471
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-07-05 14:51:13 +0100

    pixel wavelt transforms fwdback test

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -8,8 +8,21 @@ def wvlttransform(L, B, J_min, Nside):
     return WaveletTransform(L, B, J_min, Nside)
 
 
-@pytest.mark.parametrize("in_type, out_type", [("harmonic_mw", "harmonic_mw"), ("harmonic_hp", "harmonic_hp")])
+@pytest.mark.parametrize(
+    "in_type, out_type",
+    [("harmonic_mw", "harmonic_mw"), ("harmonic_hp", "harmonic_hp")],
+)
 def test_wavelet_lm_fwdback(simpledata_lm, wvlttransform, in_type, out_type):
     X_wav = wvlttransform.forward(simpledata_lm, in_type=in_type, out_type=out_type)
     data_rec = wvlttransform.inverse(X_wav, in_type=in_type, out_type=out_type)
     assert np.allclose(simpledata_lm, data_rec)
+
+
+@pytest.mark.parametrize(
+    "in_type, out_type",
+    [("harmonic_mw", "harmonic_mw"), ("harmonic_hp", "harmonic_hp")],
+)
+def test_wavelet_pix_fwdback(simpledata, wvlttransform, in_type, out_type):
+    X_wav = wvlttransform.forward(simpledata, in_type=in_type, out_type=out_type)
+    data_rec = wvlttransform.inverse(X_wav, in_type=in_type, out_type=out_type)
+    assert np.allclose(simpledata, data_rec)
