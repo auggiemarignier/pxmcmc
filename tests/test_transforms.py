fea7b1f7ae8dfd9ab8eb0c6ec788da3072f84f09
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-07-07 16:31:42 +0100

    dot product test for fwd adjoint

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pytest
 
+from pxmcmc.utils import flatten_mlm
+
 
 @pytest.mark.parametrize(
     "inout_type", ["harmonic_mw", "harmonic_hp", "pixel_mw", "pixel_hp"]
@@ -19,6 +21,22 @@ def test_wavelet_fwdback(
         "pixel_mw": simpledata,
         "pixel_hp": simpledata_hp,
     }
-    X_wav = wvlttransform.forward(data[inout_type], in_type=inout_type, out_type=inout_type)
+    X_wav = wvlttransform.forward(
+        data[inout_type], in_type=inout_type, out_type=inout_type
+    )
     data_rec = wvlttransform.inverse(X_wav, in_type=inout_type, out_type=inout_type)
     assert np.allclose(data[inout_type], data_rec)
+
+
+def test_wavelet_fwd_adjoint_dot(wvlttransform, simpledata, simpledata_lm):
+    # if y = Ax and g = A'f, show that f'y = g'x
+    x = np.copy(simpledata)
+    y = wvlttransform.forward(x, in_type="pixel_mw", out_type="pixel_mw")
+
+    scal_lm = np.copy(simpledata_lm)
+    wav_lm = np.column_stack([simpledata_lm for _ in range(wvlttransform.nscales)])
+    f = flatten_mlm(wav_lm, scal_lm)
+    g = wvlttransform.forward_adjoint(f, in_type="harmonic_mw", out_type="pixel_mw")
+
+    dot_diff = f.T.dot(y) - g.T.dot(x)
+    assert np.isclose(dot_diff, 0)
