7815b8dd2895a18aabd66c9a8cb79086db0b1c51
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-07-10 11:55:57 +0100

    waveletoperator gradg test

--- tests/test_forward.py
+++ tests/test_forward.py
@@ -1,7 +1,10 @@
 import pys2let
 import numpy as np
 
-from pxmcmc.utils import expand_mlm
+from pxmcmc.utils import expand_mlm, flatten_mlm
+
+# These tests mostly test implementation
+# Tests on individal transform and measurement operators are more valuable
 
 
 def test_WaveletTransformOperator_forward(swtoperator):
@@ -14,5 +17,21 @@ def test_WaveletTransformOperator_forward(swtoperator):
         L = swtoperator.transform.L
         J_min = swtoperator.transform.J_min
         expected = pys2let.synthesis_axisym_wav_mw(wav, scal, B, L, J_min)
-    
+
     assert np.allclose(swtoperator.forward(sample), expected)
+
+
+def test_WaveletTransformOperator_gradg(swtoperator):
+    preds = np.random.rand(len(swtoperator.data)).astype(np.complex)
+    if swtoperator.setting == "analysis":
+        expected = preds - swtoperator.data
+    else:
+        B = swtoperator.transform.B
+        L = swtoperator.transform.L
+        J_min = swtoperator.transform.J_min
+        diff = preds - swtoperator.data
+        expected = flatten_mlm(
+            *pys2let.synthesis_adjoint_axisym_wav_mw(diff, B, L, J_min)
+        )
+
+    assert np.allclose(swtoperator.calc_gradg(preds), expected)
