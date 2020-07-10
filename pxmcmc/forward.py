8eed46422a9ffd62328713f08d7b357884e4f49b
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-07-10 10:38:11 +0100

    restarting wavelet transform operator - need tests

--- pxmcmc/forward.py
+++ pxmcmc/forward.py
@@ -1,11 +1,4 @@
-import pys2let
-import numpy as np
-import healpy as hp
-import os
-from scipy import sparse
-
-from pxmcmc.utils import expand_mlm, flatten_mlm, alm2map, map2alm, wavelet_basis
-from pxmcmc.measurements import Identity, PathIntegral
+from pxmcmc.measurements import Identity
 from pxmcmc.transforms import WaveletTransform
 
 
@@ -56,153 +49,21 @@ class ForwardOperator:
         ) / (self.sig_d ** 2)
 
 
-class ISWTOperator(ForwardOperator):
-    """
-    Inverse spherical wavelet transfrom operator.
-    Returns the spherical harmonic coefficients.
-    Analysis: sample spherical harmonic coefficients
-    Synthesis: sample harmonic wavelet coefficeints
-    """
-
-    def __init__(self, data, sig_d, L, B, J_min, setting, dirs=1, spin=0):
+class WaveletTransformOperator(ForwardOperator):
+    def __init__(self, data, sig_d, setting, L, B, J_min, Nside=None, dirs=1, spin=0):
         super().__init__(data, sig_d, setting)
 
-        self.nparams = L * L
-
+        map_type = "pixel_mw"
         self.transform = WaveletTransform(
             L,
             B,
             J_min,
-            dirs=dirs,
-            spin=spin,
-            inv_out_type="harmonic_mw",
-            inv_in_type="harmonic_mw",
-            inv_adj_in_type="harmonic_mw",
-            inv_adj_out_type="harmonic_mw",
+            Nside,
+            dirs,
+            spin,
+            inv_in_type=map_type,
+            inv_out_type=map_type,
+            inv_adj_in_type=map_type,
+            inv_adj_out_type=map_type,
         )
-        self.measurement = Identity(M=len(self.data), N=self.nparams)
-
-
-class SWC2PixOperator(ISWTOperator):
-    """
-    Inverse spherical wavelet transfrom operator.
-    Returns pixel valyes.
-    Analysis: sample pixel values
-    Synthesis: sample harmonic wavelet coefficeints
-    """
-
-    def __init__(self, data, sig_d, Nside, L, B, J_min, setting, dirs=1, spin=0):
-        super().__init__(data, sig_d, L, B, J_min, setting, dirs, spin)
-        self.Nside = Nside
-
-        if setting == "analysis":
-            self.nparams = hp.nside2npix(Nside)
-
-    def _forward_synthesis(self, X):
-        clm = super()._forward_synthesis(X)
-        clm_hp = pys2let.lm2lm_hp(clm, self.L)
-        c = alm2map(clm_hp, self.Nside)
-        return c
-
-    def _forward_analysis(self, X):
-        return X
-
-    def _gradg_analysis(self, preds):
-        return super()._gradg_analysis(preds)
-
-    def _gradg_synthesis(self, preds):
-        diff_hp = preds - self.data
-        diff_lm_hp = map2alm(diff_hp, self.L)
-        diff_lm_mw = pys2let.lm_hp2lm(diff_lm_hp, self.L)
-        diff_mw = pys2let.alm2map_mw(diff_lm_mw, self.L, self.spin)
-
-        f_wav, f_scal = pys2let.synthesis_adjoint_axisym_wav_mw(
-            diff_mw, self.B, self.L, self.J_min
-        )
-        f_scal_lm = pys2let.map2alm_mw(f_scal, self.L, 0)
-        f_wav_lm = np.zeros((self.L ** 2, self.nscales), dtype=np.complex)
-        vlen = pys2let.mw_size(self.L)
-        for j in range(self.nscales):
-            f_wav_lm[:, j] = pys2let.map2alm_mw(
-                f_wav[j * vlen : (j + 1) * vlen + 1], self.L, 0
-            )
-        return flatten_mlm(f_wav_lm, f_scal_lm) / (self.sig_d ** 2)
-
-
-class GCPIntegralOperator(ForwardOperator):
-    def __init__(
-        self,
-        datafile,
-        Nside,
-        L,
-        B,
-        J_min,
-        setting,
-        dirs=1,
-        spin=0,
-        path_matrix_file=None,
-    ):
-        self.Nside = Nside
-        self.L = L
-        self.B = B
-        self.J_min = J_min
-        self.J_max = pys2let.pys2let_j_max(self.B, self.L, self.J_min)
-        self.nscales = self.J_max - self.J_min + 1
-        self.dirs = dirs
-        self.spin = spin
-
-        if setting == "synthesis":
-            basis = wavelet_basis(L, B, J_min, dirs, spin)
-            self.nparams = np.prod(basis.shape)
-        else:
-            self.nparams = hp.nside2npix(Nside)
-
-        self._read_datafile(datafile)
-        self._get_path_matrix(path_matrix_file)
-
-    def _read_datafile(self, datafile):
-        """
-        Expects a file with the following columns for each path:
-        start_lat, start_lon, stop_lat, stop_lon, data, error
-        Coordinates given in degrees
-        TODO: Check what happens when each data point has a different error
-        TODO: Figure out what to do with minor/major and nsim
-        """
-        all_data = np.loadtxt(datafile)
-        start_lat = all_data[:, 0]
-        start_lon = all_data[:, 1]
-        self.start = np.stack([start_lat, start_lon], axis=1)
-        stop_lat = all_data[:, 2]
-        stop_lon = all_data[:, 3]
-        self.stop = np.stack([stop_lat, stop_lon], axis=1)
-        self.data = all_data[:, 4]
-        sig_d = all_data[:, 5]
-        self.sig_d = np.max(sig_d)
-
-    def _get_path_matrix(self, path_matrix_file):
-        if path_matrix_file is None:
-            path_matrix_file = f"path_matrix_{self.Nside}.npz"
-        if os.path.exists(path_matrix_file):
-            self._read_path_matrix_file(path_matrix_file)
-        else:
-            self._build_path_matrix_file(path_matrix_file)
-
-    def _read_path_matrix_file(self, path_martix_file):
-        self.path_matrix = sparse.load_npz(path_martix_file)
-
-    def _build_path_matrix_file(self, path_matrix_file):
-        from greatcirclepaths import GreatCirclePath
-        from multiprocessing import Pool
-
-        def build_path(start, stop):
-            path = GreatCirclePath(start, stop, self.Nside)
-            path.get_points(1000)
-            path.fill()
-            return path.map
-
-        itrbl = [(start, stop) for (start, stop) in zip(self.start, self.stop)]
-        with Pool() as p:
-            result = p.starmap_async(build_path, itrbl)
-            paths = result.get()
-        self.path_matrix = sparse.csr_matrix(paths)
-        sparse.save_npz(path_matrix_file, self.path_matrix)
+        self.measurement = Identity(len(data), L * L)
