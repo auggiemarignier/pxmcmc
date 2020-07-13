8d3e5badf9ad01fb1b6ca16d7c399a33562b7091
zcfbllm <zcfbllm@login12.myriad.ucl.ac.uk>
2020-07-13 16:33:27 +0100

    input arguments

--- experiments/earthtopography/main.py
+++ experiments/earthtopography/main.py
@@ -1,18 +1,23 @@
 import healpy as hp
 import datetime
 import argparse
-import os
+import pys2let
 
 from pxmcmc.mcmc import MYULA, PxMALA, PxMCMCParams
-from pxmcmc.forward import SWC2PixOperator
+from pxmcmc.forward import WaveletTransformOperator
 from pxmcmc.prox import L1
 from pxmcmc.saving import save_mcmc
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--infile", type=str, default="ETOPO1_Ice_hpx_256.fits")
 parser.add_argument("--outdir", type=str, default=".")
-args = parser.parse_args()
+parser.add_argument("--jobid", type=str, default="0")
 
+parser.add_argument("--algo", type=str, default="myula")
+parser.add_argument("--setting", type=str, default="synthesis")
+parser.add_argument("--delta", type=float, default=1e-10)
+parser.add_argument("--mu", type=float, default=1)
+args = parser.parse_args()
 
 topo = hp.read_map(args.infile, verbose=False)
 sig_d = 0.03
@@ -21,34 +26,47 @@ L = 16
 B = 1.5
 J_min = 2
 Nside = 32
-setting = "synthesis"
-topo_d = hp.ud_grade(topo, Nside)
-forwardop = SWC2PixOperator(topo_d, sig_d, Nside, L, B, J_min, setting)
+setting = args.setting
+# topo_d = hp.ud_grade(topo, Nside)
+topo_d_lm = hp.map2alm(topo, L - 1)
+topo_d = pys2let.alm2map_mw(pys2let.lm_hp2lm(topo_d_lm, L), L, 0)
+
+forwardop = WaveletTransformOperator(topo_d, sig_d, setting, L, B, J_min)
 params = PxMCMCParams(
-    nsamples=int(1e3),
+    nsamples=int(2e4),
     nburn=0,
-    ngap=100,
+    ngap=int(1e2),
     complex=True,
-    delta=2.5e-8,
+    delta=args.delta,
     lmda=1e-7,
-    mu=1e-1,
-    verbosity=100,
+    mu=args.mu,
+    verbosity=int(1e2),
 )
-regulariser = L1(setting, None, None, params.lmda * params.mu)
+regulariser = L1(
+    setting,
+    forwardop.transform.forward,
+    forwardop.transform.inverse_adjoint,
+    params.lmda * params.mu,
+)
 
 print(f"Number of data points: {len(topo_d)}")
 print(f"Number of model parameters: {forwardop.nparams}")
 
 NOW = datetime.datetime.now()
 
-mcmc = PxMALA(forwardop, regulariser, params)
+if args.algo == "myula":
+    mcmc = MYULA(forwardop, regulariser, params)
+elif args.algo == "pxmala":
+    mcmc = PxMALA(forwardop, regulariser, params, tune_delta=True)
+else:
+    raise ValueError
 mcmc.run()
 
 save_mcmc(
     mcmc,
     params,
-    ".",
-    filename=os.path.join(args.outdir, f"pxmala_{NOW.strftime('%d%m%y_%H%M%S')}"),
+    args.outdir,
+    filename=f"{args.algo}_{NOW.strftime('%d%m%y_%H%M%S')}_{args.jobid}",
     L=L,
     B=B,
     J_min=J_min,
