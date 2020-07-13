c3744aeb047d0fb8f06a2d2c29a544482dcadb61
zcfbllm <zcfbllm@login12.myriad.ucl.ac.uk>
2020-07-13 16:32:52 +0100

    plot difference

--- experiments/earthtopography/plot.py
+++ experiments/earthtopography/plot.py
@@ -2,9 +2,12 @@ import argparse
 import h5py
 import numpy as np
 from matplotlib import cm
+import pys2let
+import healpy as hp
 
 from pxmcmc import plotting
 from pxmcmc import uncertainty
+from pxmcmc.utils import alm2map
 
 
 parser = argparse.ArgumentParser()
@@ -12,6 +15,8 @@ parser.add_argument("datafile", type=str)
 parser.add_argument("directory", type=str)
 parser.add_argument("--suffix", type=str, default="")
 args = parser.parse_args()
+
+Nside = 32
 
 
 def filename(name):
@@ -27,11 +32,24 @@ L1s = file["L1s"][()]
 evo = plotting.plot_evolution(logpi, L2s, L1s)
 evo.savefig(filename("evolution"))
 
+topo = hp.read_map(
+    "/home/zcfbllm/src_proxmcmc/experiments/earthtopography/ETOPO1_Ice_hpx_256.fits",
+    verbose=False,
+)
+truth = hp.alm2map(hp.map2alm(topo, params["L"] - 1), Nside)
 
 MAP_idx = np.where(logpi == max(logpi))
 MAP = file["predictions"][MAP_idx][0]
-maxapost = plotting.mollview(MAP, cmap=cm.jet, title="Maximum a posteriori solution")
+MAP_hp = alm2map(
+    pys2let.lm2lm_hp(pys2let.map2alm_mw(MAP, params["L"], 0), params["L"]), Nside
+)
+maxapost = plotting.mollview(
+    MAP_hp, cmap=cm.jet, title="Maximum a posteriori solution", flip="geo"
+)
 maxapost.savefig(filename("MAP"))
+
+diff = plotting.mollview(truth - MAP_hp, cmap=cm.jet, title="Truth - MAP", flip="geo")
+diff.savefig(filename("diff"))
 
 MAP_X = file["chain"][MAP_idx][0]
 mapx = plotting.plot_chain_sample(MAP_X)
@@ -39,10 +57,15 @@ mapx.savefig(filename("MAP_X"))
 
 
 basis_els = plotting.plot_basis_els(
-    file["chain"][750:], params["L"], params["B"], params["J_min"], inflate_mads=100
+    file["chain"][3000:], params["L"], params["B"], params["J_min"], inflate_mads=100
 )  # Careful with L
 basis_els.savefig(filename("basis_els"))
 
-ci_range = uncertainty.credible_interval_range(file["predictions"][750:])
-ci_map = plotting.mollview(ci_range, min=0, title="95% credible interval range")
+ci_range = uncertainty.credible_interval_range(file["predictions"][3000:])
+ci_range_hp = alm2map(
+    pys2let.lm2lm_hp(pys2let.map2alm_mw(ci_range, params["L"], 0), params["L"]), 32
+)
+ci_map = plotting.mollview(
+    ci_range_hp, min=0, title="95% credible interval range", flip="geo"
+)
 ci_map.savefig(filename("ci_map"))
