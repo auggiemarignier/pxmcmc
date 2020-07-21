249df0c3d36118507ac0fef19543a6bc85c0e3ee
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-07-21 14:23:29 +0100

    simple tests for mcmc algorithms

---
+++ tests/test_mcmc.py
@@ -0,0 +1,47 @@
+import pytest
+from pytest_cases import parametrize_with_cases
+
+from pxmcmc.mcmc import MYULA, PxMALA, SKROCK, PxMCMCParams
+from pxmcmc.measurements import Identity
+from pxmcmc.transforms import IdentityTransform
+from pxmcmc.forward import ForwardOperator
+from pxmcmc.prox import L1
+
+
+@pytest.fixture
+def forwardop(simpledata, setting, sig_d):
+    ndata = nparams = len(simpledata)
+    transform = IdentityTransform()
+    measurement = Identity(ndata, nparams)
+    return ForwardOperator(
+        simpledata, sig_d, setting, transform, measurement, nparams=nparams
+    )
+
+
+@pytest.fixture
+def prox(forwardop, setting):
+    return L1(
+        setting, forwardop.transform.inverse, forwardop.transform.inverse_adjoint, 1
+    )
+
+
+@pytest.fixture
+def mcmcparams():
+    return PxMCMCParams(nsamples=100, nburn=10, ngap=5, verbosity=0)
+
+
+def case_myula(forwardop, prox, mcmcparams):
+    return MYULA(forwardop, prox, mcmcparams)
+
+
+def case_pxmala(forwardop, prox, mcmcparams):
+    return PxMALA(forwardop, prox, mcmcparams)
+
+
+def case_skrock(forwardop, prox, mcmcparams):
+    return SKROCK(forwardop, prox, mcmcparams)
+
+
+@parametrize_with_cases("algo", cases=".")
+def test_algorithms(algo):
+    algo.run
