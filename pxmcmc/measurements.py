115307de54afc555337c5cb0829a6fff24c1ea88
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-09-14 12:36:36 +0100

    better variable names

--- pxmcmc/measurements.py
+++ pxmcmc/measurements.py
@@ -5,13 +5,13 @@ class Measurement:
     """
     Measurement operators operate on some map X to create observations Y
     Adjoints operate on measurements to return some map
-    X is a vector of size N (number of parameters in analysis setting)
-    Y is a vector of size M (number of data points)
+    X is a vector of size npix (number of pixels on the sphere)
+    Y is a vector of size ndata (number of data points)
     """
 
-    def __init__(self, M, N):
-        self.M = M
-        self.N = N
+    def __init__(self, ndata, npix):
+        self.ndata = ndata
+        self.npix = npix
 
     def forward(self):
         raise NotImplementedError
@@ -21,32 +21,32 @@ class Measurement:
 
 
 class Identity(Measurement):
-    def __init__(self, M, N):
-        super().__init__(M, N)
-        self.eye = sparse.eye(self.M, self.N)
+    def __init__(self, ndata, npix):
+        super().__init__(ndata, npix)
+        self.eye = sparse.eye(self.ndata, self.npix)
         self.eye_adj = self.eye.getH()
 
     def forward(self, X):
-        assert len(X) == self.N
+        assert len(X) == self.npix
         return self.eye.dot(X)
 
     def adjoint(self, Y):
-        assert len(Y) == self.M
+        assert len(Y) == self.ndata
         return self.eye_adj.dot(Y)
 
 
 class PathIntegral(Measurement):
-    def __init__(self, datafile, M, N, path_matrix_file=None):
-        super().__init__(M, N)
+    def __init__(self, datafile, ndata, npix, path_matrix_file=None):
+        super().__init__(ndata, npix)
         self._read_datafile(datafile)
         self._get_path_matrix(path_matrix_file)
 
         self.path_matrix_adj = self.path_matrix.getH()
 
     def forward(self, X):
-        assert len(X) == self.N
+        assert len(X) == self.npix
         return self.path_matrix.dot(X)
 
     def adjoint(self, Y):
-        assert len(Y) == self.M
+        assert len(Y) == self.ndata
         return self.path_matrix_adj.dot(Y)
