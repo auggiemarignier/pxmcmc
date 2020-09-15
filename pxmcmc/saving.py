93eb74fd721d53476ef0578b9332a6300439f7c1
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-09-15 15:50:42 +0100

    options for what dataset to save to save space

--- pxmcmc/saving.py
+++ pxmcmc/saving.py
@@ -2,13 +2,25 @@ import os
 import h5py
 
 
-def save_mcmc(mcmc, params, outpath, filename="outputs", **kwargs):
+def save_mcmc(
+    mcmc,
+    params,
+    outpath,
+    filename="outputs",
+    L2s=True,
+    L1s=True,
+    predictions=True,
+    **kwargs,
+):
     with h5py.File(os.path.join(outpath, f"{filename}.hdf5"), "w") as f:
         f.create_dataset("logposterior", data=mcmc.logPi)
-        f.create_dataset("predictions", data=mcmc.preds)
+        if predictions:
+            f.create_dataset("predictions", data=mcmc.preds)
         f.create_dataset("chain", data=mcmc.chain)
-        f.create_dataset("L2s", data=mcmc.L2s)
-        f.create_dataset("L1s", data=mcmc.L1s)
+        if L2s:
+            f.create_dataset("L2s", data=mcmc.L2s)
+        if L1s:
+            f.create_dataset("L1s", data=mcmc.L1s)
         if hasattr(mcmc, "acceptance_trace"):
             f.create_dataset("acceptances", data=mcmc.acceptance_trace, dtype="i1")
         if hasattr(mcmc, "deltas_trace"):
