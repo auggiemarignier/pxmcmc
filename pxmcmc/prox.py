107c97959bec1119ef78b2c9d481bb483ef912ef
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-09-25 12:41:00 +0100

    new class for weighted L1 norm

--- pxmcmc/prox.py
+++ pxmcmc/prox.py
@@ -1,10 +1,15 @@
-from pxmcmc.utils import soft
+import numpy as np
+import pys2let
+import pyssht
+
+from pxmcmc.utils import soft, weighted_s2
 
 
 class L1:
     """
     L1-norm regulariser
     """
+
     def __init__(self, setting, fwd, adj, T):
         """
         setting = "analysis" or "synthesis"
@@ -29,3 +34,33 @@ class L1:
 
     def _proxf_analysis(self, X):
         return X + self.fwd(soft(self.adj(X), self.T) - self.adj(X))
+
+
+class S2_Wavelets_L1(L1):
+    """
+    L1 regulariser for wavelets on S2 (MW sampling)
+    Performs some weighting to avoid overemphasizing pixels at the poles
+    """
+
+    def __init__(self, setting, fwd, adj, T, L, B, J_min):
+        super().__init__(setting, fwd, adj, T)
+        self.L = L
+        B = B
+        J_min = J_min
+        J_max = pys2let.pys2let_j_max(B, L, J_min)
+        self.nscales = J_max - J_min + 1
+
+    def proxf(self, X):
+        X = self._weight_maps(X, self.L)
+        return super().proxf(X)
+
+    def _weight_maps(self, X, L):
+        X_w = np.zeros_like(X)
+        if self.setting == "synthesis":
+            map_size = pyssht.sample_length(L, Method="MW")
+            for j in range(self.nscales):
+                wav_map = X[j * map_size : (j + 1) * map_size]
+                X_w[j * map_size : (j + 1) * map_size] = weighted_s2(wav_map, self.L)
+        else:
+            X_w = weighted_s2(X, self.L)
+        return X_w
--- tests/test_proxes.py
+++ tests/test_proxes.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from pxmcmc.prox import L1
+from pxmcmc.prox import L1, S2_Wavelets_L1
 
 
 @pytest.fixture
@@ -19,3 +19,31 @@ def test_L1(L1regulariser):
 
     X = np.arange(100)
     assert np.alltrue(L1regulariser.proxf(X) == soft(X, L1regulariser.T))
+
+
+@pytest.fixture
+def S2_Wavs_L1reg(setting, L, B, J_min):
+    reg = S2_Wavelets_L1(setting, None, None, 0.5, L, B, J_min)
+
+    def identity(X):
+        if setting == "analysis":
+            return np.matmul(np.eye(L * (2 * L - 1)), X)
+        else:
+            return np.matmul(np.eye(reg.nscales * L * (2 * L - 1)), X)
+
+    reg.fwd = identity
+    reg.adj = identity
+    return reg
+
+
+def test_S2_Wavlets_L1(S2_Wavs_L1reg, simpledata):
+    """
+    Since the soft thresholding and weighting have been tested individually
+    just make sure it runs
+    """
+    if S2_Wavs_L1reg.setting == "analysis":
+        S2_Wavs_L1reg.proxf(simpledata)
+    else:
+        S2_Wavs_L1reg.proxf(
+            np.concatenate([simpledata for _ in range(S2_Wavs_L1reg.nscales)])
+        )
