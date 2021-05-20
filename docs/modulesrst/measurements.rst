b600de3ef167ff5792d5db57258d23ce06fe03e7
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-05-20 15:53:56 +0100

    measurements module docs

--- docs/modulesrst/measurements.rst
+++ docs/modulesrst/measurements.rst
@@ -1,5 +1,11 @@
 Measurements
 ============
 
+Measurement operators operate on some image `X` to predict observations `Y`.  Adjoints operate on measurements to return some image.
+
+.. note::
+   
+   We use the terms `image` and `pixels` in this documentation because the code was originally written for 2D imaging problems.  In general, `pixels` should be taken to mean `parameters` in the space in which the measurement operator gets applied.
+
 .. automodule:: measurements
     :members: 
--- pxmcmc/measurements.py
+++ pxmcmc/measurements.py
@@ -3,39 +3,63 @@ from scipy import sparse
 
 class Measurement:
     """
-    Measurement operators operate on some map X to create observations Y
-    Adjoints operate on measurements to return some map
-    X is a vector of size npix (number of pixels on the sphere)
-    Y is a vector of size ndata (number of data points)
-    """
+    Base class.
 
+    :param int ndata: number of observed data points
+    :param int npix: number of pixels in image
+    """
     def __init__(self, ndata, npix):
         self.ndata = ndata
         self.npix = npix
 
-    def forward(self):
+    def forward(self, X):
+        """
+        Forward modelling from image to observations.  Implemented by user in custom child class.
+
+        :param X: image
+        :return: observations
+        """
         raise NotImplementedError
 
-    def adjoint(self):
+    def adjoint(self, Y):
+        """
+        Adjoint modelling from observations to image.  Not necessarily the inverse of :meth:`self.forward`.  Implemented by user in custom child class.
+
+        :param Y: observations
+        :return: image
+        """
         raise NotImplementedError
 
 
 class Identity(Measurement):
+    """
+    Identity measurement operator i.e. what goes in comes out.
+    """
     def __init__(self, ndata, npix):
         super().__init__(ndata, npix)
         self.eye = sparse.eye(self.ndata, self.npix)
         self.eye_adj = self.eye.getH()
 
     def forward(self, X):
+        """:meta private:"""
         assert len(X) == self.npix
         return self.eye.dot(X)
 
     def adjoint(self, Y):
+        """:meta private:"""
         assert len(Y) == self.ndata
         return self.eye_adj.dot(Y)
 
 
 class PathIntegral(Measurement):
+    """
+    Path integration using a matrix that describes a set of paths.
+
+    .. todo::
+       Since this is just a matrix multiplication, can be renamed to something more generic.
+
+    :param path_matrix: :math:`N_{\mathrm{paths}}\\times N_{\mathrm{pix}}` matrix describing a set of paths.
+    """
     def __init__(self, path_matrix):
         self.path_matrix = path_matrix
         self.path_matrix_adj = self.path_matrix.getH()
@@ -43,9 +67,11 @@ class PathIntegral(Measurement):
         self.ndata, self.npix = path_matrix.shape
 
     def forward(self, X):
+        """:meta private:"""
         assert len(X) == self.npix
         return self.path_matrix.dot(X)
 
     def adjoint(self, Y):
+        """:meta private:"""
         assert len(Y) == self.ndata
         return self.path_matrix_adj.dot(Y)
