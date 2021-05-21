326636c0d30800a0722a3960fa785d93ec811cf0
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-05-21 09:58:43 +0100

    saving module docs

--- docs/modulesrst/saving.rst
+++ docs/modulesrst/saving.rst
@@ -1,5 +1,7 @@
 Saving
 ============
 
+This module gives a single function for saving the results of an MCMC run.  Everything gets saved in a :code:`.hdf5` file using `h5py <https://www.h5py.org/>`_.
+
 .. automodule:: saving
    :members: 
--- pxmcmc/saving.py
+++ pxmcmc/saving.py
@@ -5,6 +5,15 @@ import h5py
 def save_mcmc(
     mcmc, params, outpath, filename="outputs", **kwargs,
 ):
+    """
+    Saves the MCMC run in a :code:`.hd5f` file. Any variable defined by a :class:`mcmc.PxMCMC` instance's :code:`tracking` option will be saved as a `dataset <https://docs.h5py.org/en/stable/high/dataset.html>`_.  Runtime parameters and other things that the user may wish to save are saved as  `attributes <https://docs.h5py.org/en/stable/high/attr.html>`_.
+
+    :param mcmc: type :class:`mcmc.PxMCMC` after its :meth:`run` has been called.
+    :param params: type :class:`mcmc.PxMCMCParams` used to setup the :code:`mcmc` instance.
+    :param string outpath: directory in which file should be saved.
+    :param string filename: filename without extension.
+    :param \**kwargs: additional bits users want to save.
+    """
     with h5py.File(os.path.join(outpath, f"{filename}.hdf5"), "w") as f:
         if hasattr(mcmc, "logPi"):
             f.create_dataset("logposterior", data=mcmc.logPi)
